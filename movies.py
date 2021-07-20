1d6656ccb9d861dd48022f1b914437cce601d71c
sujivennapusa <vennapusasuji463@gmail.com>
2021-07-20 22:07:24 +0530

    opeations on list1

---
+++ movies.py
@@ -0,0 +1,11 @@
+movies=["bahubali","mission mangal","dhruva",123,678,5.5]
+del movies[4]
+movies.remove(5.5)
+language=["hindi","telugu","english"]
+print(movies+language)
+print(len(movies))
+print(language*5)
+language.pop(1)
+print(language)
+language[0]="english"
+print(language)
