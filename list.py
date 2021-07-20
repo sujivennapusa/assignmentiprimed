81b7f298468768156f67a51a056cf9721c11aab1
sujivennapusa <vennapusasuji463@gmail.com>
2021-07-20 21:51:19 +0530

    opeations on list

---
+++ list.py
@@ -0,0 +1,13 @@
+emp=["aparna","jyoshna","madhavi",123,146,99]
+del emp[2]
+emp.remove(123)
+company=["prodapt","iprimed","infosys"]
+print(len(emp+company))
+print(emp+company)
+print(company*2)
+print(emp)
+print(len(emp))
+emp.pop(1)
+print(emp)
+company[0]="accenture"
+print(company)
